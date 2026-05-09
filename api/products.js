50d2a777130949c5dcce15f5b0bed739641579b6
juliohuallanca99-lgtm <juliohuallanca99@gmail.com>
2026-05-08 23:17:17 -0500

    Fix: use correct env variable names

--- api/products.js
+++ api/products.js
@@ -1,6 +1,9 @@
 import { Redis } from "@upstash/redis";
 
-const redis = Redis.fromEnv();
+const redis = new Redis({
+  url: process.env.UPSTASH_KV_REST_API_URL,
+  token: process.env.UPSTASH_KV_REST_API_TOKEN,
+});
 
 const KEY = "products";
 
